0eceee508cf1b6cefc00b55e73c951d6b6101e13
João Sousa <1210631@isep.ipp.pt>
2026-03-14 00:08:05 +0000

    Initial code repo

---
+++ SmartAlarms/src/main/java/isep/smartalarms/SmartAlarmsApplication.java
@@ -0,0 +1,13 @@
+package isep.smartalarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAlarmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAlarmsApplication.class, args);
+    }
+
+}
---
+++ SmartAlarms/src/test/java/isep/smartalarms/SmartAlarmsApplicationTests.java
@@ -0,0 +1,13 @@
+package isep.smartalarms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAlarmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
